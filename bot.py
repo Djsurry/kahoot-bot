2078d094f5fa6360847657d5972bc70e6510ac0d
David Surry <djsurry@gmail.com>
2019-01-07 01:30:27 -0500

    first

---
+++ bot.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from time import sleep
+from socket import socket
+import PySimpleGUI as sg  
+path = '/Users/davidsurry/Downloads/chromedriver'
+INPUT_ID = 'inputSession'
+NAME_ID = 'username'
+
+class Browser:
+    def __init__(self, pin, name):
+        self.name = name
+        self.pin = pin
+        self.browser = webdriver.Chrome(path)
+
+    def run(self):
+        self.browser.get('https://kahoot.it/')
+        elem = self.browser.find_element_by_id(INPUT_ID)
+        elem.send_keys(self.pin + '\n')
+        sleep(1)
+        elem = self.browser.find_element_by_id(NAME_ID)
+        elem.send_keys(self.name + '\n')
+
+
+class Window:
+    def __init__(self):
+        self.layout = [[sg.Text('Welcome to Kahoot-Bot!', key='intro')],
+                       [sg.Text('Game Code'), sg.input(key='code')]
+                       [sg.Text('Number of bots (no more than 100)'), sg.Input(key='number')],
+                       [sg.Text('Custom names *optional* (text file of names, each on own line)')],
+                       [sg.Input(key='file', do_not_clear=True), sg.FileBrowse()],
+                       [sg.OK(), sg.Exit()]]
+        self.window = sg.Window('Window that stays open').Layout(self.layout)
+
+    def run(self):
+        while True:
+            event, values = self.window.Read()
+            if event is None or event == 'Exit':
+                break
+            if values['code'] == '':
+                sg.Popup('Please enter a game-code')
+                continue
+            else:
+                code = values['code']
+            try:
+                number = int(values['number'])
+            except ValueError:
+                sg.Popup('Please enter a number for the number field')
+                continue
+            if 1 > number or number > 100:
+                sg.Popup('Please enter a number bigger than 0 and less than 100')
+            if values['file'] != '':
+                try:
+                    with open(values['file']) as f:
+                        names = [n.strip() for n in f.readlines()]
+                except FileNotFoundError:
+                    sg.Popup('File {} not found'.format(values['file']))
+                    continue
+            else:
+                names = None
+            self.window.FindElement('intro').Update('Working')
+            r = self.order_botting(code, names, number)
+
+            if r is None:
+                sg.Popup('Unknown Error Occured')
+            elif r == 1:
+                sg.Popup('Success')
+            elif r == 0:
+                sg.Popup('Failure: Bad code')
+
+    @staticmethod
+    def order_botting(code, names, number):
+        sock = socket()
+        sock.connect(('167.99.180.229', 5555))
+        sock.sendall(f'{code}\x00{names}\x00{number}\x00'.encode())
+        resp = sock.recv(1)
+        if not resp:
+            return None
+        elif resp == b'\x00':
+            return 0
+        elif resp == b'\x01':
+            return 1
+
+
+
+
+
+w = Window()
+w.run()
---
+++ server.py
@@ -0,0 +1,116 @@
+import socket
+from selenium import webdriver
+import selenium
+from time import sleep
+import threading
+import names
+
+path = '/Users/davidsurry/Downloads/chromedriver'
+INPUT_ID = 'inputSession'
+NAME_ID = 'username'
+
+
+class Browser:
+    def __init__(self, pin, name):
+        self.name = name
+        self.pin = pin
+        self.browser = webdriver.Chrome(path)
+    def run(self):
+        self.browser.get('https://kahoot.it/')
+        elem = self.browser.find_element_by_id(INPUT_ID)
+        elem.send_keys(self.pin + '\n')
+        sleep(1)
+        elem = self.browser.find_element_by_id(NAME_ID)
+        elem.send_keys(self.name + '\n')
+    def test(self):
+        self.browser.get('https://kahoot.it/')
+        elem = self.browser.find_element_by_id(INPUT_ID)
+        elem.send_keys(self.pin + '\n')
+        sleep(1)
+        try:
+            elem = self.browser.find_element_by_id(NAME_ID)
+            return True
+        except selenium.common.exceptions.NoSuchElementException:
+            return False
+
+
+
+class Host(threading.Thread):
+    def __init__(self, conn):
+        super().__init__()
+        self.sock = conn
+
+    def read(self):
+        code =[]
+        names = []
+        number = []
+        seen = 0
+        while True:
+            data = self.sock.recv(1)
+            if data == '\x00':
+                seen += 1
+                if seen == 3:
+                    break
+                continue
+            if seen == 0:
+                code.append(data)
+            elif seen == 1:
+                names.append(data)
+            elif seen == 2:
+                number.append(data)
+
+        return b''.join(code).decode(), b''.join(names).decode(), b''.join(number).decode()
+
+    def run(self):
+        try:
+            code, names, number = self.read()
+        except:
+            self.sock.close()
+            return
+        b = Botnet(code, names, number)
+        if b.test():
+            self.sock.sendall('\x01')
+        else:
+            self.sock.sendall('\x00')
+        b.start()
+        self.sock.close()
+
+class Server:
+    def __init__(self):
+        self.port = 55555
+        self.sock = socket.socket()
+        self.sock.bind(('', self.port))
+        self.sock.listen(5)
+
+    def run(self):
+        conn, addr = self.sock.accept()
+        h = Host(conn)
+        h.start()
+
+
+class Botnet:
+    def __init__(self, code, names, number):
+        self.code = code
+        self.names = names.split() if names else Botnet.gen_names()
+        self.number = number
+
+    def test(self):
+        b = Browser(self.code, self.names[0])
+        return b.test()
+
+    def start(self):
+        created = 0
+        index = 0
+        max_index = len(self.names)-1
+        while created < self.number:
+            b = Browser(self.code, self.names[index])
+            b.run()
+            index += 1
+            if index > max_index:
+                index = 0
+            created += 1
+
+    @staticmethod
+    def gen_names():
+        return [names.get_first_name() for _ in range(20)]
+
